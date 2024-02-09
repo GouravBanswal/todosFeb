5b9ddee99a6b7ab9d6bdacaa2f622876699b3474
ashutosh1998github <81618033+ashutosh1998github@users.noreply.github.com>
2024-02-09 19:27:20 +0530

    design Added

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -4,7 +4,7 @@ const Todo = require("../model/Todo")
 exports.getTodos = async (req,res) => {
     try {
         const result = await Todo.find()
-        res.status(200).json({message: "Fetch Success", result})
+        res.status(200).json({message: "FATCH Success", result})
     } catch (error) {
         res.status(500).json({message: error.message ||"somthing went wrong"})
     }
@@ -12,22 +12,22 @@ exports.getTodos = async (req,res) => {
 exports.addTodos = async(req,res) => {
     try {
         await Todo.create(req.body)
-        res.status(201).json({message: "add Success"})
+        res.status(201).json({message: "ADD Success"})
     } catch (error) {
         res.status(500).json({message: error.message ||"somthing went wrong"})
     }
 }
 exports.updateTodos = async(req,res) => {
     try {
-        await Todo.findByIDAndUpdate(req,params.id, req.body)
-        res.status(200).json({message: "update Success"})
+        await Todo.findByIDAndUpdate(req.params.id, req.body)
+        res.status(200).json({message: "UPDATE Success"})
     } catch (error) {
         res.status(500).json({message: error.message ||"somthing went wrong"})
     }
 }
 exports.deleteTodos = async(req,res) => {
     try {
-        await Todo.findByIdAndDelete(req,params.id)
+        await Todo.findByIdAndDelete(req.params.id)
         res.status(200).json({message: "delete Success"})
     } catch (error) {
         res.status(500).json({message: error.message ||"somthing went wrong"})
